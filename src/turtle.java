4150fce7e34a3b560059e223e5bed64a864e90d3
Oscar Mok <oscar97123@gmail.com>
2021-03-05 19:49:01 +0800

    Added random turtle moving speed & initialize size

--- src/turtle.java
+++ src/turtle.java
@@ -14,12 +14,15 @@ public class turtle implements Runnable{
     private final JPanel fishBowl_JPanel;
     private final JLabel Turtle_JLabel;
     private final int mouse_X_point, mouse_Y_point;
+    private int turtle_width = 70; //鳥龜的最小闊度為70
+    private int turtle_height = 60; //鳥龜的最小高度為60
     private int randomSpeed;
     private int turtle_direction;
     private Image turtle_left, turtle_right;
     private int x_position_now ,y_position_now;
     private SecureRandom secureRandom = new SecureRandom();
     private boolean turtle_touch_the_ground = false; //一開始鳥龜還未接觸到地面
+    private int count = 0;
 
     public turtle(int x, int y, JLabel Turtle_JLabel, JPanel fishBowl_JPanel){
         this.fishBowl_JPanel = fishBowl_JPanel;
@@ -33,18 +36,20 @@ public class turtle implements Runnable{
         y_position_now = mouse_Y_point;
 
 
+        turtle_width += secureRandom.nextInt(40); //隨機鳥龜的闊度
+        turtle_height += secureRandom.nextInt(40); //隨機鳥龜的高度
         turtle_direction = secureRandom.nextInt(2);
 
         read_turtle_image();
-        random_turtle_direction_n_rescale(turtle_direction);
+        random_turtle_direction_n_rescale(turtle_width, turtle_height, turtle_direction);
 
         randomSpeed = secureRandom.nextInt(80) + 5; //隨機烏龜的速度
     }
 
-    private void random_turtle_direction_n_rescale(int direction_int) {
+    private void random_turtle_direction_n_rescale(int width, int height, int direction_int) {
 
-        turtle_left = turtle_left.getScaledInstance(96, 74, Image.SCALE_DEFAULT); //Resize 圖片
-        turtle_right = turtle_right.getScaledInstance(96, 74, Image.SCALE_DEFAULT);
+        turtle_left = turtle_left.getScaledInstance(width, height, Image.SCALE_DEFAULT); //Resize 圖片
+        turtle_right = turtle_right.getScaledInstance(width, height, Image.SCALE_DEFAULT);
 
         switch (direction_int){
             case 0:
@@ -88,6 +93,15 @@ public class turtle implements Runnable{
 
                 turtle_reach_border(); //當烏龜到達邊界時
 
+                if (count == 200){ //隔一段時間重新 random 速度 (每執行第200次)
+
+                    turtle_width = turtle_width -10;
+                    turtle_height = turtle_height -10;
+                    random_turtle_direction_n_rescale(turtle_width, turtle_height, turtle_direction); //套回function 改變烏龜面向的方向
+
+                    count = 0; //重算
+                }
+
                 Thread.sleep(randomSpeed); //以thread sleep來控制烏龜的速度
             }
         } catch (InterruptedException e) {
@@ -119,12 +133,12 @@ public class turtle implements Runnable{
     private void turtle_reach_border() {
         if (x_position_now > fishBowl_JPanel.getWidth() - Turtle_JLabel.getWidth()){ //扣掉烏龜的闊度 才能在烏龜碰到邊緣時馬上回頭
             turtle_direction = 0; //往右遊
-            random_turtle_direction_n_rescale(turtle_direction); //套回function 改變烏龜面向的方向
+            random_turtle_direction_n_rescale(turtle_width, turtle_height, turtle_direction); //套回function 改變烏龜面向的方向
         }
 
         if (x_position_now < 0){
             turtle_direction = 1; //往左遊
-            random_turtle_direction_n_rescale(turtle_direction); //套回function 改變烏龜面向的方向
+            random_turtle_direction_n_rescale(turtle_width, turtle_height, turtle_direction); //套回function 改變烏龜面向的方向
         }
     }
 }
